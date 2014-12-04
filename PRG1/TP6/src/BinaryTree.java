d24e0065d1836066a22f7f257b0bef98833a1906
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2014-12-04 14:15:23 +0100

    Adding what i found in the courses of PRG1 about the TP6 BinaryTree class

--- PRG1/TP6/src/BinaryTree.java
+++ PRG1/TP6/src/BinaryTree.java
@@ -1,159 +1,255 @@
 import java.util.Stack;
 
+
+/**
+ * The binary tree class
+ */
 public class BinaryTree <T> {
 
-    /*
-      arbre binaire de <Item> :
-      chainage references pour les fils : Element left, right
-      pile des peres : Stack<Element> pile
-    */
+	//Attributes
+	private Element root;
 
-    private class Element{
-	// element de ArbreBinaire <Item>
-	public T value;
-	public Element left, right;
-	public Element() 
-	{
-	    value = null;
-	    left = null;
-	    right = null;
-	}
-	public boolean isEmpty()
-	{
-	    return left == null && right == null;
-	}
-    }
 
-    private Element root;
-
-    public BinaryTree() {
-    }
-
-    public TreeIterator iterator ()
-    {
-	return new TreeIterator();
-    }
-
-    public boolean isEmpty()
-    {
-	return false;
-    }
-
-    public class TreeIterator implements Iterator<T>
-    {
-	private Element currentNode;
-	private Stack<Element> stack;
-
-	private TreeIterator() {
-	    stack = new Stack<Element>();
-	    currentNode = root;
+	/**
+	 * Constructor of binary tree
+	 */
+	public BinaryTree() {
+		//Just create the root
+		root = new Element();
 	}
 
-	@Override public void goLeft() {
-	    try	{
-		assert !this.isEmpty() : "le butoir n'a pas de fils";
-		assert currentNode.left != null : "le fils gauche d'existe pas";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
+
+	/**
+	 * Method to get the iterator
+	 */
+	public TreeIterator iterator() {
+		return new TreeIterator();
 	}
 
-	@Override public void goRight() {
-	    try {
-		assert !this.isEmpty() : "le butoir n'a pas de fils";
-		assert currentNode.right != null : "le fils droit d'existe pas";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
+
+	/**
+	 * Check if empty or not
+	 * @return true if empty, false if not
+	 */
+	public boolean isEmpty() {
+		return this.iterator().isEmpty();
 	}
 
-	@Override public void goUp() {
-	    try {
-		assert !stack.empty() :" la racine n'a pas de p?e";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
+
+
+	/* ################################### Inner classes ############################## */
+	/**
+	 * The Iterator class
+	 */
+	public class TreeIterator implements Iterator<T> {
+
+		//Attributes
+		private Element currentNode;
+		private Stack<Element> stack;
+
+
+		/**
+		 * Constructor for the iterator
+		 */
+		private TreeIterator() {
+			stack = new Stack<Element>();
+			currentNode = root;
+		}
+
+
+		@Override
+		/**
+		 * Go to the left son
+		 */
+		public void goLeft() {
+			try	{
+				assert !this.isEmpty() : "Le butoir n'a pas de fils";
+				assert currentNode.left != null : "Le fils gauche n'existe pas";
+
+				p.push(currentNode);
+				currentNode = currentNode.left;
+			}
+			catch (AssertionError e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+
+
+		@Override
+		/**
+		 * Go to the right son
+		 */
+		public void goRight() {
+			try {
+				assert !this.isEmpty() : "Le butoir n'a pas de fils";
+				assert currentNode.right != null : "Le fils droit d'existe pas";
+
+				p.push(currentNode);
+				currentNode = currentNode.right;
+			}
+			catch (AssertionError e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+
+
+		@Override
+		/**
+		 * Go to the father
+		 */
+		public void goUp() {
+			try {
+				assert !stack.empty() :"La racine n'a pas de pere";
+
+				currentNode = stack.pop();
+			}
+			catch (AssertionError e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+
+
+		@Override
+		/**
+		 * Go to the root
+		 */
+		public void goRoot() {
+			Element tmp = null;
+			while (!stack.empty()) {
+				tmp = stack.pop();
+			}
+			if (tmp != null)
+				currentNode = tmp;
+			else
+				System.out.println("Arbre vide!");
+		}
+
+
+		@Override
+		/**
+		 * Check if empty or not
+		 * @return true if empty, false it not
+		 */
+		public boolean isEmpty() {
+			return (currentNode.left == null && currentNode.right == null);
+		}
+
+
+		@Override public NodeType nodeType() {
+			return NodeType.BUTOIR;
+		}
+
+		@Override public void remove() {
+			try {
+				assert !this.isEmpty() : "retirer : Butoir!";
+				assert nodeType() != NodeType.DOUBLE : "retirer : retrait d'un noeud double non permis";
+				Element newNode = null;
+
+				switch (this.nodeType()) {
+					case LEAF:
+						newNode = new Element();
+						break;
+
+					case SIMPLE-LEFT:
+						newNode = currentNode.left;
+						break;
+
+					case SIMPLE-RIGHT:
+						newNode = currentNode.right;
+						break;
+				}
+
+				currentNode.value = newNode.value;
+				currentNode.left = newNode.left;
+				currentNode.right = newNode.right;
+			}
+			catch (AssertionError e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+
+		@Override public void clear() {
+		}
+
+		@Override public T getValue() {
+			return null;
+		}
+
+		@Override public void addValue(T v) {
+			try {
+			assert isEmpty() : "Ajouter : on n'est pas sur un butoir";
+			}
+			catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+			}
+		}
+
+		@Override public void setValue(T v) {
+		}
+
+		private void ancestor(int i, int j) {
+			try {
+			assert !stack.empty() : "switchValue : argument trop grand";
+			}
+			catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+			}
+			Element x = stack.pop();
+			if (j<i) {
+			ancestor(i, j+1);
+			}
+			else {
+			T v = x.value;
+			x.value = currentNode.value;
+			currentNode.value = v;
+			}
+			stack.push(x);
+		}
+
+		@Override public void switchValue(int i) {
+			try {
+			assert i>= 0 : "switchValue : argument n?atif";
+			}
+			catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+			}
+			if (i>0) {
+			ancestor(i, 1);
+			}
+		}
 	}
 
-	@Override public void goRoot() {
-	}
 
-	@Override public boolean isEmpty() {
-	    return false;
-	}
+	/**
+	 * The Element class
+	 */
+	private class Element {
+		// element de ArbreBinaire <Item>
+		public T value;
+		public Element left, right;
 
-	@Override public NodeType nodeType() {
-	    return NodeType.BUTOIR;
-	}
+		/**
+		 * The constructor
+		 */
+		public Element() {
+			value = null;
+			left = null;
+			right = null;
+		}
 
-	@Override public void remove() {
-	    try {
-		assert nodeType() != NodeType.DOUBLE : 
-		"retirer : retrait d'un noeud double non permis";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
+		/**
+		 * Check if empty or not
+		 * @return true if empty, false it not
+		 */
+		public boolean isEmpty() {
+			return (left == null && right == null);
+		}
 	}
-
-	@Override public void clear() {
-	}
-
-	@Override public T getValue() {
-	    return null;
-	}
-
-	@Override public void addValue(T v) {
-	    try {
-		assert isEmpty() : "Ajouter : on n'est pas sur un butoir";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
-	}
-
-	@Override public void setValue(T v) {
-	}
-
-	private void ancestor(int i, int j) {
-	    try {
-		assert !stack.empty() : "switchValue : argument trop grand";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
-	    Element x = stack.pop();
-	    if (j<i) {
-		ancestor(i, j+1);
-	    }
-	    else {
-		T v = x.value;
-		x.value = currentNode.value;
-		currentNode.value = v;
-	    }
-	    stack.push(x);
-	}
-
-	@Override public void switchValue(int i) {
-	    try {
-		assert i>= 0 : "switchValue : argument n?atif";
-	    }
-	    catch (AssertionError e) {
-		e.printStackTrace();
-		System.exit(0);
-	    }
-	    if (i>0) {
-		ancestor(i, 1);
-	    }
-	}
-    }
 }
