9b8dc7305a2d123040160bb71777f10ea57a2b17
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2015-01-29 10:05:42 +0100

    End of the first part

--- SYR2/TP2/shell1.c
+++ SYR2/TP2/shell1.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
 
 // Constants
 #define MAX_SIZE 1024  // Buffer size
@@ -23,8 +25,8 @@
 int main() {
 
 	// Create the buffer to read the command entered
-	char* command = malloc(MAX_SIZE);
-	char* result_read = malloc(MAX_SIZE);
+	char *command = malloc(MAX_SIZE);
+	char *result_read = malloc(MAX_SIZE);
 
 	// Try to read the command
 	result_read = fgets(command, MAX_SIZE/sizeof(char), stdin);
@@ -34,15 +36,6 @@ int main() {
 
 		// While we have another value than exit
 		while (strcmp(command, "exit\n") != 0) {
-
-			// Display the command passed
-			printf("%s", command);
-
-			// Clear the buffer
-			bzero(command, MAX_SIZE);
-
-			// Read another one
-			result_read = fgets(command, MAX_SIZE/sizeof(char), stdin);
 
 			// If there was an error during the reading
 			if (result_read == NULL) {
@@ -55,9 +48,41 @@ int main() {
 			// If no error, we can execute the command
 			else {
 
-				
+				// Do the fork here
+				pid_t result_fork = fork();
+
+				// If the SON
+				if (result_fork == 0) {
+
+					// Clean the command ("\n" problem)
+					command[strlen(command) - 1] = '\0';
+
+					// Execute the function passed
+					return execlp(command, command, NULL);
+				}
+
+
+				// If the FATHER
+				else {
+
+					// Wait the son to terminate
+					int status;
+					pid_t result_wait;
+					while ((result_wait = wait(&status)) > 0) { }
+				}
 			}
+
+
+			// Clear the buffer
+			bzero(command, MAX_SIZE);
+
+			// Read another one
+			result_read = fgets(command, MAX_SIZE/sizeof(char), stdin);
 		}
+
+		// If the first command is exit
+		if (strcmp(command, "exit\n") != 0)
+			free(command);
 
 		// If exit enterred
 		free(result_read);
