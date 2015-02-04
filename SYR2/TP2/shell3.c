ee90c5883901ac8b51cd8e67a728b018b3bc710c
Andriamilanto Tompoariniaina <contact@napi.fr>
2015-02-04 23:26:00 +0100

    Quite done

--- SYR2/TP2/shell3.c
+++ SYR2/TP2/shell3.c
@@ -17,6 +17,14 @@
 
 
 
+/**
+ * Get a command splitted
+ *
+ * Parameters:
+ *     - char *command  => The command string to split
+ *     - char **command_parts  => A table of the splitted command
+ *
+ */
 void get_command_parts(char *command, char **command_parts) {
 	// Clean the command (remove the annoying "\n" at the end of it)
 	command[strlen(command) - 1] = '\0';
@@ -55,8 +63,17 @@ int main() {
 	fprintf(stdout, "%s\n", "Please enter the first command:");
 	char *result_read = fgets(command, (MAX_SIZE/sizeof(char)), stdin);
 
+	// Close if exit
+	if (strcmp(command, "exit\n") == 0)
+		return 0;
+
+	// Try to read the second command
 	fprintf(stdout, "%s\n", "Please enter the second command:");
 	char *result_read2 = fgets(command2, (MAX_SIZE/sizeof(char)), stdin);
+
+	// Close if exit
+	if (strcmp(command2, "exit\n") == 0)
+		return 0;
 
 	// Check if there wasn't any error
 	if ((result_read != NULL) && (result_read2 != NULL)) {
@@ -93,28 +110,63 @@ int main() {
 					get_command_parts(command, command_parts);
 					get_command_parts(command2, command_parts2);
 
-					// Execute the first command passed
-					int result_execution = execvp(command_parts[0], command_parts);
-
-					// Redirect the standard output to the standard input
-					if (dup2(0, 1) < 0) {  // Less than 0 if error
-						fprintf(stderr, "%s\n", "The io redirection failed.");
+					// Create a pipe here
+					int fd[2];
+					if (pipe(fd) < 0) {
+						fprintf(stderr, "%s\n", "Error during the creation of the pipe.");
 						return 1;
 					}
 
-					// If an error occured
-					if (result_execution != 0) {
-						fprintf(stderr, "%s%d\n", "Error during the execution of the first command. Exit with code ", result_execution);
-						return result_execution;
+					// Then create another fork
+					pid_t son_fork = fork();
+
+					// If the new son
+					if (son_fork == 0) {
+
+						// Close the unused write end
+						close(fd[1]);
+
+						// Redirect the standard input from the pipe
+						if (dup2(0, fd[0]) < 0) {  // Less than 0 if error
+							fprintf(stderr, "%s\n", "The io redirection failed in the out => in direction.");
+							return 1;
+						}
+
+						// Then execute the second one
+						int second_execution = execvp(command_parts2[0], command_parts2);
+
+						// If an error occured
+						if (second_execution != 0) {
+							fprintf(stderr, "%s%d\n", "Error during the execution of the second command. Exit with code ", second_execution);
+							return second_execution;
+						}
 					}
 
-					// Then execute the second one
-					result_execution = execvp(command_parts2[0], command_parts2);
+					// If the first son
+					else {
 
-					// If an error occured
-					if (result_execution != 0) {
-						fprintf(stderr, "%s%d\n", "Error during the execution of the second command. Exit with code ", result_execution);
-						return result_execution;
+						// Close unused read end
+						close(fd[0]);
+
+						// Redirect the standard output to the pipe
+						if (dup2(1, fd[1]) < 0) {  // Less than 0 if error
+							fprintf(stderr, "%s\n", "The io redirection failed in the in => out direction.");
+							return 1;
+						}
+
+						// Execute the first command passed
+						int result_execution = execvp(command_parts[0], command_parts);
+
+						// If an error occured
+						if (result_execution != 0) {
+							fprintf(stderr, "%s%d\n", "Error during the execution of the first command. Exit with code ", result_execution);
+							return result_execution;
+						}
+
+						// Wait the second son to terminate
+						int status;
+						pid_t result_wait;
+						while ((result_wait = wait(&status)) > 0) { }
 					}
 
 					// If no error during all the process
@@ -125,7 +177,7 @@ int main() {
 				// If the FATHER
 				else {
 
-					// Wait the son to terminate
+					// Wait the sons to terminate
 					int status;
 					pid_t result_wait;
 					while ((result_wait = wait(&status)) > 0) { }
