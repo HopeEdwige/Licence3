31bea6effd9951d53e3a404e5b36be799373c66e
Andriamilanto Tompoariniaina <contact@napi.fr>
2015-02-11 23:32:30 +0100

    I give up, we got an infinite loop after the second command ...

--- SYR2/TP2/shell3.c
+++ SYR2/TP2/shell3.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <wait.h>
+#include <sys/wait.h>
 
 
 // Constants
@@ -50,10 +50,20 @@ void get_command_parts(char *command, char **command_parts) {
 /**
  * The main function to be thrown
  *
+ * Parameters:
+ *     - int argc  => The number of arguments
+ *     - char** args  => The arguments
+ *
  * Return:
  *     - int  => The result of the execution
  */
-int main() {
+int main(int argc, char** args) {
+
+	// If there are too much arguments
+	if (argc > 1) {
+		fprintf(stderr, "%s\n", "There are too many arguments. This program requires none.");
+		return 1;  // Exit with an error code
+	}
 
 	// Create the buffer to read the command entered
 	char command[MAX_SIZE];
@@ -75,8 +85,8 @@ int main() {
 	if (strcmp(command2, "exit\n") == 0)
 		return 0;
 
-	// While we have another value than exit
-	while ((strcmp(command, "exit\n") != 0) && (strcmp(command2, "exit\n") != 0)) {
+	// While we have another value than exit (the comparison is done under)
+	while (1) {
 
 		// If there was an error during the reading
 		if (result_read == NULL) {
@@ -128,26 +138,24 @@ int main() {
 				}
 
 				// Close after we made dup on it
-				close(fd[1]);
+				//close(fd[1]);
 
 				// Then execute the second one
 				int result_execution = execvp(command_parts[0], command_parts);
 
 				// If an error occured
-				if (result_execution != 0)
-					fprintf(stderr, "%s%d\n", "Error during the execution of the first command. Exit with code ", result_execution);
-				
+				fprintf(stderr, "%s%d\n", "Error during the execution of the first command. Exit with code ", result_execution);
 				return result_execution;
 			}
 
 			// If the FATHER
-			else {
+			// Wait the first son to terminate
+			int status;
+			pid_t result_wait;
+			while ((result_wait = wait(&status)) > 0) {}
 
-				// Wait the first son to terminate
-				int status;
-				pid_t result_wait;
-				while ((result_wait = wait(&status)) > 0) { }
-			}
+
+			/* #################### SECOND COMMAND #################### */
 
 			// Then create another fork
 			pid_t son_fork;
@@ -169,26 +177,21 @@ int main() {
 				}
 
 				// Close after we made dup on it
-				close(fd[0]);
+				//close(fd[0]);
 
 				// Then execute the second one
 				int second_execution = execvp(command_parts2[0], command_parts2);
 
 				// If an error occured
-				if (second_execution != 0)
-					fprintf(stderr, "%s%d\n", "Error during the execution of the second command. Exit with code ", second_execution);
-
+				fprintf(stderr, "%s%d\n", "Error during the execution of the second command. Exit with code ", second_execution);
 				return second_execution;
 			}
 
 			// If the father
-			else {
-
-				// Wait the last son to terminate
-				int status;
-				pid_t result_wait;
-				while ((result_wait = wait(&status)) > 0) { }
-			}
+			// Wait the last son to terminate
+			int status_bis;
+			pid_t result_wait_bis;
+			while ((result_wait_bis = wait(&status_bis)) > 0) {}
 		}
 
 		// Break a line
