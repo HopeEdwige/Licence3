3e9803c4b387eafe0ac158880f17551e6fec5ffc
Andriamilanto Tompoariniaina <tompo.andri@gmail.com>
2015-02-12 09:56:36 +0100

    Adding the new TP3 of SYR2

---
+++ SYR2/TP3/README.txt
@@ -0,0 +1,15 @@
+Andriamilanto Tompoariniaina
+Boucherie Thomas
+
+Question 1: 
+	Ce programme créer un segment de memoire partagé à un key prédifini (le même pour toutes les instances du programme) et l'attache au processus.
+
+Question 2:
+	Il ne créer pas de processus fils et n'a donc pas besoin de partager la mémoire partagée avec des fils.
+
+Question 3:
+	Le programme ne detruit pas le segment de memoire partagé il le detache du procesus en cours. 
+	Si on appel le programme plusieurs fois un seul segment est créé et memorisé.
+
+Question 4:
+	Les differents procesus ecrient en meme temps sur le tableau et donc sur la memoire partagée ce qui nous donne une ecriture concurante entre les differents procesus.
---
+++ SYR2/TP3/nettoie.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <strings.h>
+
+#define TAILLE 1024
+
+
+/*void ecrire_tableau(int *compteur, char *tableau) {
+  char message[64], *msg=message;
+  snprintf(message, 64, "Je suis le processus %d!\n", getpid());
+
+  while ((*compteur<TAILLE)&&(*msg)) {
+    tableau[*compteur] = *msg;
+    msg++;
+    usleep(100000);
+    (*compteur)++;
+  }
+}
+*/
+
+int main() {
+  //int id; //*compteur;
+  //char *tableau;
+
+  int id = shmget((key_t)1234,TAILLE+sizeof(int),0600|IPC_CREAT);  //Taille = Tableau et sizeof(int) = compteur
+  //if (id<0) { perror("Error shmget"); exit(1); }
+
+  //compteur = (int*) shmat(id,0,0);
+  //if (compteur==NULL) { perror("Error shmat"); exit(1); }
+
+  //tableau = (char *)(compteur + 1);
+  
+  //ecrire_tableau(compteur, tableau);
+  //printf("%s\n", tableau);
+
+  //if (shmdt(compteur)<0) { perror("Error shmdt"); exit(1); }
+
+  if (shmctl(id, IPC_RMID, NULL) < 0) { perror("Erreur shmctl"); exit(1); }
+  return 0;
+
+}
